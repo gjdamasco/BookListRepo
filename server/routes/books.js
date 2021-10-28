4dd0fe8b0dd0e1cb4f9ca3dc7db657ab4d695524
gjdamasco <gjcenina@gmail.com>
2021-10-27 20:53:10 -0400

    DELETE button done

--- server/routes/books.js
+++ server/routes/books.js
@@ -100,7 +100,7 @@ router.post('/:id', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
-    
+
      let id = req.params.id;
 
      let updatedBook = book({
@@ -124,8 +124,6 @@ router.post('/:id', (req, res, next) => {
              res.redirect('/books');
          }
      });
-     
-
 });
 
 // GET - process the delete by user id
@@ -134,8 +132,23 @@ router.get('/delete/:id', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+     let id = req.params.id;
      
-   
+     console.log(id);
+
+     book.remove({_id: id}, (err) => {
+
+      if(err)
+      {
+          console.log(err);
+          res.end(err);
+      }
+      else
+      {
+           // refresh the book list
+           res.redirect('/books');
+      }
+    });   
 });
 
 
