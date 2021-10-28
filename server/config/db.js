8257d2e08351ce9f9ae6dfee35580ee4b63f7f5a
gjdamasco <gjcenina@gmail.com>
2021-10-27 20:40:58 -0400

    first commit ADD done

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb://localhost/books"
+  //"URI": "mongodb+srv://geejay:6wryCEitfUsNc4hK@geejay.6mjhr.mongodb.net/book_fav?retryWrites=true&w=majority"
+  
+};
